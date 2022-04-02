3d1e6fe67b87360209c4da61128155a5eaacfdc7
Artem Kudenikov <a.kudenikov@yandex.ru>
2022-04-02 11:47:17 +0300

    error log added

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,7 @@
 module.exports = (err, req, res, next) => {
   const status = err.statusCode || 500;
+  // eslint-disable-next-line no-console
+  console.log(err.stack || err);
   const { message } = err;
   res.status(status).json({ err: message || 'Произошла ошибка на сервере' });
   return next();
